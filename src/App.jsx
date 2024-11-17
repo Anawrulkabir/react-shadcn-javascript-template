78442a2a88851b6dbea399c094d9fa1b06786882
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2024-11-17 13:27:18 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Button>Hello</Button>
+    </div>
+  )
+}
+
+export default App
